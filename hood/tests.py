634a175472f61dd38427ccfe583c35ea3602df80
rehemafaith <rehemafaith01@gmail.com>
2019-06-05 14:21:55 +0300

    added styling

--- hood/tests.py
+++ hood/tests.py
@@ -6,4 +6,10 @@ from .models import Business,Area,Updates
 
 class areaTestClass(TestCase):
   def setUp(self):
-      self.nairobi = Area()
+      self.nairobi = Area(name = 'Nairobi', population = '25')
+
+  def test_instance(self):
+    self.asserTrue(isinstance(self.nairobi,Area))
+
+  def test_save_method(self):
+    
